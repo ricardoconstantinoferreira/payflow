05816d5db38848ef4255cf4dbc25315e172bc1ee
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-05-28 00:20:03 -0300

    second commit

---
+++ src/main/java/com/flow/payflow/controller/TransactionController.java
@@ -0,0 +1,12 @@
+package com.flow.payflow.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/transaction/payment", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TransactionController {
+
+
+}
---
+++ src/test/java/com/flow/payflow/PayflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.payflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
