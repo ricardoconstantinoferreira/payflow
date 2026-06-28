5177c7d4cde5e0d5d8aaef0dfd39d497ed074c45
Ricardo Constantino Ferreira <ricardo.ferreira@publicisgroupe.com>
2026-06-28 01:03:35 -0300

    config and implementing the rabbitmq to work with queue

---
+++ src/main/java/com/flow/payflow/component/PaymentConsumer.java
@@ -0,0 +1,22 @@
+package com.flow.payflow.component;
+
+import com.flow.payflow.config.rabbitmq.RabbitMQConfig;
+import com.flow.payflow.dto.TransactionDto;
+import com.flow.payflow.service.TransactionService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentConsumer {
+
+    private final TransactionService transactionService;
+
+    public PaymentConsumer(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_PAYMENT)
+    public void consumePaymentMessage(TransactionDto transactionDto) {
+        this.transactionService.create(transactionDto);
+    }
+}
